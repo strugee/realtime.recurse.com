baa9e3658efb527407e90960356640e02737747a
Alex Jordan <alex@strugee.net>
2017-02-23 15:49:01 -0500

    Get a very basic file watcher working

--- client/bin/daemon.py
+++ client/bin/daemon.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python
+
+import time
+from os import path
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class ProjectEventHandler(FileSystemEventHandler):
+    def on_any_event(self, event):
+        print(event.src_path)
+
+event_handler = ProjectEventHandler()
+observer = Observer()
+observer.schedule(event_handler, path='.', recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop()
+
+observer.join()
